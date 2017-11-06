7ddf2b6fe003787cb6cf60300776273751651b41
msolorio <msolorio789@gmail.com>
2017-11-05 19:26:52 -0700

    calculate a winner and update message

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,8 +14,34 @@ export default class Board extends React.Component {
     this.handleSquareClick = this.handleSquareClick.bind(this);
   }
 
+  calculateWinner() {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    const squareVals = this.state.squareVals;
+
+    return winningCombos.reduce((winner, combo) => {
+      const [a, b, c] = combo;
+      if (squareVals[a] === squareVals[b] && squareVals[a] === squareVals[c]) {
+        return squareVals[a];
+      }
+      return winner;
+    }, null);
+  }
+
   handleSquareClick(index) {
+    if (this.calculateWinner()) return;
+
     if (this.state.squareVals[index] !== null) return;
+
     const newSquareVals = this.state.squareVals.slice();
     newSquareVals[index] = this.state.xIsNext ? 'X' : 'O';
 
@@ -31,15 +57,22 @@ export default class Board extends React.Component {
     return (
       <Square value={val}
         onSquareClick={handleSquareClick}
-        key={index}
-      />
+        key={index} />
     );
   };
 
+  getMessage() {
+    if (this.calculateWinner()) {
+      return `Winner: ${this.calculateWinner()}`;
+    }
+    return `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+  }
+
   render() {
+
     return (
       <div className="Board">
-        <h2>Next Player: {this.state.xIsNext ? 'X' : 'O'}</h2>
+        <h2>{this.getMessage()}</h2>
         <div className="boardRow">
           {
             this.state.squareVals.map((squareVal, index) => {
@@ -47,7 +80,6 @@ export default class Board extends React.Component {
             })
           }
         </div>
-
       </div>
     );
   }
